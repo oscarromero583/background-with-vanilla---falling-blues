b7083188dd553fda94d086d4649c547996b10e82
oscarromero583 <obetaoscar583@gmail.com>
2024-11-09 08:47:40 +0100

    final commit 	modified:   main.js

--- main.js
+++ main.js
@@ -4,6 +4,42 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const bluefalls = [];
+const bluefalls = []; 
 
-function initBlueFalls
+function initBlueFalls() {
+    for (let i = 0; i < 100; i++) {
+        bluefalls.push({ 
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            length: Math.random() * 20 + 5,
+            speed: Math.random() * 10 + 5,
+            thickness: Math.random() * 2 + 1,
+        });
+    }
+}
+
+function drawFalling() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (const bluefall of bluefalls) { 
+        ctx.beginPath();
+        ctx.moveTo(bluefall.x, bluefall.y);
+        ctx.lineTo(bluefall.x, bluefall.y + bluefall.length);
+        ctx.strokeStyle = '#00f';
+        ctx.lineWidth = bluefall.thickness;
+        ctx.stroke();
+
+       
+        bluefall.y += bluefall.speed;
+
+   
+        if (bluefall.y > canvas.height) {
+            bluefall.y = 0 - bluefall.length;
+        }
+    }
+
+    requestAnimationFrame(drawFalling);
+}
+
+initBlueFalls();
+drawFalling();
