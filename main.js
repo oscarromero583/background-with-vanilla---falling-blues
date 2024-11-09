f9bd082a3fa4bcb9c68683730f532e0b0cfd8e04
oscarromero583 <obetaoscar583@gmail.com>
2024-11-09 07:37:47 +0100

    new feature modified:   main.js 	modified:   style.css

---
+++ main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('effects');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const bluefalls = [];
+
+function initBlueFalls
